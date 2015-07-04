bb422514ff99f915752b9f61b72de4c5110b2e17
Sakamoto, Kazunori <exkazuu@gmail.com>
2015-07-04 15:12:33 +0900

    Add comments.

--- src/main/java/jp/ac/nii/Main.java
+++ src/main/java/jp/ac/nii/Main.java
@@ -13,7 +13,12 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) throws InstantiationException,
 			IllegalAccessException, IOException {
-		// 実行すると result.csv ファイルが作成されます。
+		// 以下の式の分母を計算します。
+		// 関連度 = 商品Xと商品Yのペアの総数 / 商品Xを含むペアの総数
+		// 出力結果例：
+		//		ごま団子,5313
+		//		カスタードシュークリーム,5162
+		//		クリームあんみつ,5249		
 		System.out.println("-------- First Stage --------");
 		Job<String, String, Integer> mapReduce = new Job<String, String, Integer>();
 		mapReduce.setMapper(DenominatorCalcMapper.class);
@@ -25,6 +30,16 @@ public class Main {
 				new File("denominator.csv").toPath(),
 				StandardCopyOption.REPLACE_EXISTING);
 
+		// 以下の式の分子を計算します。
+		// 関連度 = 商品Xと商品Yのペアの総数 / 商品Xを含むペアの総数
+		// 出力結果例：
+		//		あんドーナツ,ところてん,152
+		//		あんドーナツ,みつまめ,158
+		//		あんドーナツ,オレンジピールチョコレート,125
+		//      ...
+		//      ところてん,あんドーナツ,152
+		//
+		// 注意：ペアの順序をひっくり返したデータも出力に含めてください。
 		System.out.println();
 		System.out.println("-------- Second Stage --------");
 		Job<String, String, Integer> mapReduce2 = new Job<String, String, Integer>();
@@ -37,6 +52,14 @@ public class Main {
 				new File("numerator.csv").toPath(),
 				StandardCopyOption.REPLACE_EXISTING);
 
+		// 以下の式の関連度を計算します。
+		// 関連度 = 商品Xと商品Yのペアの総数 / 商品Xを含むペアの総数
+		// 出力結果例：
+		//		ごま団子,うぐいすあんぱん,0.025032938076416336
+		//		ごま団子,ねりようかん,0.02973837756446452
+		//		ごま団子,カスタードシュークリーム,0.031055900621118012
+		//
+		// 注意：関連度が0.025を超える場合のみ出力してください
 		System.out.println();
 		System.out.println("-------- Third Stage --------");
 		Job<String, String, String> mapReduce3 = new Job<String, String, String>();
@@ -45,6 +68,8 @@ public class Main {
 		mapReduce3.setNumberOfLinesPerMapper(100);
 		mapReduce3.setNumberOfReducers(5);
 
+		// denominator.csv と numerator.csv を結合して読み込みます。
+		// 正しく別が付くように、前者は各行に「D」を後者は各行に「N」を付加します。
 		List<String> lines = new ArrayList<String>();
 		List<String> denominatorLines = readTextFile("denominator.csv");
 		List<String> numeratorLines = readTextFile("numerator.csv");
