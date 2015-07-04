3033acb3c8c4bc8f95151300f5f9e83b9bbeb241
Sakamoto, Kazunori <exkazuu@gmail.com>
2015-07-04 13:23:45 +0900

    Update WordCountReducer.java

--- src/main/java/jp/ac/nii/WordCountReducer.java
+++ src/main/java/jp/ac/nii/WordCountReducer.java
@@ -2,6 +2,6 @@ package jp.ac.nii;
 
 import java.util.List;
 
-public class WordCountReducer extends Reducer<String, Integer> {
+public class WordCountReducer extends /* TODO: 適切に実装してください。 */ {
 	// TODO: 適切に実装してください。
 }
