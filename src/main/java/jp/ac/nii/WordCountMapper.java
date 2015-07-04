724bfa8a821198f88e646a52680211ffea49d26b
Sakamoto, Kazunori <exkazuu@gmail.com>
2015-07-04 11:54:56 +0900

    Remove some code.

--- src/main/java/jp/ac/nii/WordCountMapper.java
+++ src/main/java/jp/ac/nii/WordCountMapper.java
@@ -3,25 +3,5 @@ package jp.ac.nii;
 import java.util.List;
 
 public class WordCountMapper extends Mapper<String, String, Integer> {
-	@Override
-	public void map(List<String> lines) {
-		// emit と isWord を使ってワードカウントのMapを実装してください。
-		for (String line : lines) {
-			String[] words = line.split(" ");
-			for (String word : words) {
-				if (isWord(word)) {
-					emit(word, 1);
-				}
-			}
-		}
-	}
-
-	private boolean isWord(String word) {
-		for (int i = 0; i < word.length(); i++) {
-			if (!Character.isAlphabetic(word.charAt(i))) {
-				return false;
-			}
-		}
-		return word.length() > 0;
-	}
+	// TODO: 適切に実装してください。
 }
--- src/main/java/jp/ac/nii/WordCountReducer.java
+++ src/main/java/jp/ac/nii/WordCountReducer.java
@@ -3,12 +3,5 @@ package jp.ac.nii;
 import java.util.List;
 
 public class WordCountReducer extends Reducer<String, Integer> {
-	@Override
-	protected void reduce(String key, List<Integer> values) {
-		int sum = 0;
-		for (Integer value : values) {
-			sum += value;
-		}
-		write(key, sum);
-	}
+	// TODO: 適切に実装してください。
 }
