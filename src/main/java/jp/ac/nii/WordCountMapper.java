0e083c78cd0f9fd6e9ab42820a619dcb1240ab92
Sakamoto, Kazunori <exkazuu@gmail.com>
2015-07-04 13:23:30 +0900

    Update WordCountMapper.java

--- src/main/java/jp/ac/nii/WordCountMapper.java
+++ src/main/java/jp/ac/nii/WordCountMapper.java
@@ -2,6 +2,6 @@ package jp.ac.nii;
 
 import java.util.List;
 
-public class WordCountMapper extends Mapper<String, String, Integer> {
+public class WordCountMapper extends /* TODO: 適切に実装してください。 */ {
 	// TODO: 適切に実装してください。
 }
